b6aca60ec405176d1a3c1b16347c309fd750d8f6
VieiraGuilherme-dev <gv524003@gmail.com>
2025-08-12 21:25:18 -0300

    Conectando a MongoDB com repository e service

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.example.workshopmongo.domain;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
