cf7bd1344c99863c8e93c462a3239859374cbd06
VieiraGuilherme-dev <gv524003@gmail.com>
2025-08-14 18:31:07 -0300

    Obtendo post por id

---
+++ src/main/java/com/example/workshopmongo/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.example.workshopmongo.resources;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.dto.UserDTO;
+import com.example.workshopmongo.services.UserService;
+import com.example.workshopmongo.services.exception.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
+
---
+++ src/main/java/com/example/workshopmongo/services/exception/PostService.java
@@ -0,0 +1,24 @@
+package com.example.workshopmongo.services.exception;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.dto.UserDTO;
+import com.example.workshopmongo.repository.PostRepository;
+import com.example.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+}
